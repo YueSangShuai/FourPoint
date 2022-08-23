d85baffc61125e6be35e7aa0d0ef127ca400e165
59781 <597810815@qq.com>
2022-08-23 15:28:17 +0800

    增加视频流

--- detect_face.py
+++ detect_face.py
@@ -34,7 +34,7 @@ def scale_coords_landmarks(img1_shape, coords, img0_shape, ratio_pad=None):
     coords[:, [0, 2, 4, 6, 8]] -= pad[0]  # x padding
     coords[:, [1, 3, 5, 7, 9]] -= pad[1]  # y padding
     coords[:, :10] /= gain
-    #clip_coords(coords, img0_shape)
+    # clip_coords(coords, img0_shape)
     coords[:, 0].clamp_(0, img0_shape[1])  # x1
     coords[:, 1].clamp_(0, img0_shape[0])  # y1
     coords[:, 2].clamp_(0, img0_shape[1])  # x2
@@ -47,28 +47,30 @@ def scale_coords_landmarks(img1_shape, coords, img0_shape, ratio_pad=None):
     coords[:, 9].clamp_(0, img0_shape[0])  # y5
     return coords
 
+
 def show_results(img, xyxy, conf, landmarks, class_num):
-    h,w,c = img.shape
+    h, w, c = img.shape
     tl = 1 or round(0.002 * (h + w) / 2) + 1  # line/font thickness
     x1 = int(xyxy[0])
     y1 = int(xyxy[1])
     x2 = int(xyxy[2])
     y2 = int(xyxy[3])
-    #cv2.rectangle(img, (x1,y1), (x2, y2), (0,255,0), thickness=tl, lineType=cv2.LINE_AA)
+    # cv2.rectangle(img, (x1,y1), (x2, y2), (0,255,0), thickness=tl, lineType=cv2.LINE_AA)
 
-    clors = [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(0,255,255)]
+    clors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255)]
 
     for i in range(5):
+        if i==2:
+            continue
         point_x = int(landmarks[2 * i])
         point_y = int(landmarks[2 * i + 1])
-        cv2.circle(img, (point_x, point_y), tl+1, clors[i], -1)
+        cv2.circle(img, (point_x, point_y), tl + 1, clors[i], -1)
 
     tf = max(tl - 1, 1)  # font thickness
     label = str(int(class_num)) + ': ' + str(conf)[:5]
     print('label', str(int(class_num)) + ': ' + str(conf)[:5])
     cv2.putText(img, label, (x1, y1 - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
     return img
-
 
 
 def detect_one(model, image_path, device):
@@ -83,7 +85,7 @@ def detect_one(model, image_path, device):
     h0, w0 = orgimg.shape[:2]  # orig hw
     r = img_size / max(h0, w0)  # resize image to img_size
     if r != 1:  # always resize down, only resize up if training with augmentation
-        interp = cv2.INTER_AREA if r < 1  else cv2.INTER_LINEAR
+        interp = cv2.INTER_AREA if r < 1 else cv2.INTER_LINEAR
         img0 = cv2.resize(img0, (int(w0 * r), int(h0 * r)), interpolation=interp)
 
     imgsz = check_img_size(img_size, s=model.stride.max())  # check img_size
@@ -133,15 +135,105 @@ def detect_one(model, image_path, device):
     cv2.imwrite('result.jpg', orgimg)
 
 
+def detect_video(model, image, device, image_size, conf_thres, iou_thres):
+    from cv2 import getTickCount, getTickFrequency
+    loop_start = getTickCount()
+    # Load model
+    img_size = image_size
+    conf_thres = conf_thres
+    iou_thres = iou_thres
+
+    orgimg = copy.deepcopy(image)  # BGR
+    img0 = copy.deepcopy(orgimg)
+    assert orgimg is not None, 'Image Not Found '
+    h0, w0 = orgimg.shape[:2]  # orig hw
+    r = img_size / max(h0, w0)  # resize image to img_size
+    if r != 1:  # always resize down, only resize up if training with augmentation
+        interp = cv2.INTER_AREA if r < 1 else cv2.INTER_LINEAR
+        img0 = cv2.resize(img0, (int(w0 * r), int(h0 * r)), interpolation=interp)
+
+    imgsz = check_img_size(img_size, s=model.stride.max())  # check img_size
+
+    img = letterbox(img0, new_shape=imgsz)[0]
+    # Convert
+    img = img[:, :, ::-1].transpose(2, 0, 1).copy()  # BGR to RGB, to 3x416x416
+
+    # Run inference
+    t0 = time.time()
+
+    img = torch.from_numpy(img).to(device)
+    img = img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+
+    # Inference
+    t1 = time_synchronized()
+    pred = model(img)[0]
+
+    # Apply NMS
+    pred = non_max_suppression_face(pred, conf_thres, iou_thres)
+
+    print('img.shape: ', img.shape)
+    print('orgimg.shape: ', orgimg.shape)
+
+    # Process detections
+    for i, det in enumerate(pred):  # detections per image
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], orgimg.shape).round()
+
+            # Print results
+            for c in det[:, -1].unique():
+                n = (det[:, -1] == c).sum()  # detections per class
+
+            det[:, 5:15] = scale_coords_landmarks(img.shape[2:], det[:, 5:15], orgimg.shape).round()
+
+            for j in range(det.size()[0]):
+                xyxy = det[j, :4].view(-1).tolist()
+                conf = det[j, 4].cpu().numpy()
+                landmarks = det[j, 5:15].view(-1).tolist()
+                class_num = det[j, 15].cpu().numpy()
+                orgimg = show_results(orgimg, xyxy, conf, landmarks, class_num)
+    loop_time = cv2.getTickCount() - loop_start
+    total_time = loop_time / (cv2.getTickFrequency())  # 使用getTickFrequency()更加准确
+    running_FPS = int(1 / total_time)  # 帧率取整
+    cv2.putText(orgimg, str(running_FPS), (50, 300), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 255), 2)
+    cv2.imshow('result.jpg', orgimg)
+    cv2.waitKey(10)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=r'D:\Porject\Python_project\FourPoint\runs\train\exp4\weights\best.pt', help='model.pt path(s)')
-    parser.add_argument('--image', type=str, default=r"E:\Robotmaster\rm\images\12.jpg", help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--weights', nargs='+', type=str,
+                        default=r'runs/train/exp/weights/best.pt',
+                        help='model.pt path(s)')
+    parser.add_argument('--image', type=str, default=r"E:\Robotmaster\rm\images\12.jpg",
+                        help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img_size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--video', action='store_false', default=r"C:\Users\59781\Desktop\1.mp4", help='using video')
+    parser.add_argument('--conf_thres', type=float, default=0.3, help='')
+    parser.add_argument('--iou_thres', type=float, default=0.5, help='')
+
     opt = parser.parse_args()
     print(opt)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = load_model(opt.weights, device)
-    detect_one(model, opt.image, device)
+    if opt.video:
+
+        cap = cv2.VideoCapture(opt.video)
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')  # 视频编解码器
+        fps = cap.get(cv2.CAP_PROP_FPS)  # 帧数
+        width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 宽高
+        out = cv2.VideoWriter('result.mp4', fourcc, fps, (width, height))  # 写入视频
+        while cap.isOpened():
+
+            ret, frame = cap.read()
+            out.write(frame)  # 写入帧
+            detect_video(model,frame,device,opt.img_size,opt.conf_thres,opt.iou_thres)
+
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
+    else:
+        detect_one(model, opt.image, device)
